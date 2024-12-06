e486fba6632bf71bc5a19efdf5b81e6ffe08d54b
Wasif <muhammadwasif2027@gmail.com>
2024-12-07 00:10:21 +0500

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'login_screen.dart';
+import 'screens/weatherscreen.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(); // Initialize Firebase
-  runApp(const MyApp());
+void main() {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Weather App',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const LoginScreen(),
+      home: WeatherScreen(),
     );
   }
 }
+
